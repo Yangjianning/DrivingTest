5bb92709366df814d5c5f024d0a0c5aead7ae1f6
yang <2387633348@qq.com>
2017-06-20 09:23:17 +0800

    my first commit

---
+++ app/src/main/java/Activity/KeYiFrament.java
@@ -0,0 +1,30 @@
+package Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yang.drivingschool.R;
+
+/**
+ * Created by yang on 17-6-19.
+ */
+
+public class KeYiFrament extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_keyi, null);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
